7872d5a5c48bf649dcd1d22cc926c1352f3ae42d
kjwinfield <katherine.winfield@addenbrookes.nhs.uk>
2023-01-16 13:59:30 +0000

    fix typos

--- resources/home/dnanexus/pull_from_opencga.py
+++ resources/home/dnanexus/pull_from_opencga.py
@@ -60,12 +60,12 @@ def extract_case_information(case, study):
         for phenotype in proband['phenotypes']:
             phenotype_list.append(phenotype['id'])
 
-    # Retrieve "disorder" and "date evaluateed which would be required for API
-    # submission to ClinVar if this functionality via a push_to_clinvar.py
-    # script were added to eggd_pandora in the future
+    # Retrieve "disorder" and "date evaluated" which would be required for API
+    # submission to ClinVar if this functionality were added to eggd_pandora in
+    # the future via a push_to_clinvar.py script.
     # These are commented out as this functionality does not exist. If this
     # functionality were added in the future, these variables would be added to
-    # case_dict
+    # case_dict.
 
     # if proband['disorders']:
     #     disorder = proband['disorders'][0]['id']
