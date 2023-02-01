ff117f32e0ee60ae0c9c4ecbebf93b16f4500e7a
kjwinfield <katherine.winfield@addenbrookes.nhs.uk>
2023-02-01 11:16:58 +0000

    add pull_to_opencga tests

--- resources/home/dnanexus/tests/test_pandora.py
+++ resources/home/dnanexus/tests/test_pandora.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import pytest
 from push_to_decipher import calculate_zygosity, calculate_variant_type, format_variant_json_for_decipher
+from pull_from_opencga import extract_proband_sex, extract_proband_phenotypes, extract_proband_variants, format_required_data_into_case_json
 
 
 class TestDecipher():
@@ -112,7 +113,7 @@ class TestDecipher():
             'chr': '12',
             'start': '21912765',
             'ref_sequence': 'G',
-            'alt_sequence': 'GAA',
+            'alt_sequence': 'GA',
             'inheritance': 'unknown',
             'genotype': 'heterozygous',
             'can_be_public': False
@@ -127,7 +128,7 @@ class TestDecipher():
             'chr': '12',
             'start': '21912765',
             'ref_sequence': 'G',
-            'alt_sequence': 'GA',
+            'alt_sequence': 'GAA',
             'inheritance': 'unknown',
             'genotype': 'heterozygous',
             'can_be_public': False
@@ -173,11 +174,11 @@ class TestDecipher():
             'chr': '12',
             'start': '21912765',
             'ref_sequence': 'G',
-            'alt_sequence': 'GTTT',
+            'alt_sequence': 'GT',
             'inheritance': 'unknown',
             'genotype': 'heterozygous',
             'can_be_public': False
-            }
+                }
             }
         },
         {'data': {'type': 'Variant',
@@ -188,21 +189,96 @@ class TestDecipher():
             'chr': '12',
             'start': '21912765',
             'ref_sequence': 'G',
-            'alt_sequence': 'GT',
+            'alt_sequence': 'GTTT',
             'inheritance': 'unknown',
             'genotype': 'heterozygous',
             'can_be_public': False
-                }
             }
-        }
+            }
+        },
     ]
+
 
 class TestOpenCGA():
     '''
-    Tests for checking function to push data to DECIPHER works
+    Tests for checking function to pull data from OpenCGA
     '''
+    proband = {"id": "12345", "sex": {"id": "MALE"},
+                "phenotypes": [
+                    {
+                        "id": "HP:0000119"
+                    },
+                    {
+                        "id": "HP:0000121"
+                    },
+                    {
+                        "id": "HP:0000377"
+                    }
+                ]
+            }
+    interpretation = [
+        {'type': "INDEL", "studies": [
+            {"samples": [{"data": ["0/1"]}],
+             "files": [{"call": {"variantId": "1:10108:C:CT"}}]
+            }]
+        },
+        {'type': "SNV", 'id': "1:927003:C:T", "studies": [
+            {"samples":[{'data': ['1|1']}]},
+        ]
+        }
+    ]
+
+    def test_proband_sex_extraction(self):
+        '''
+        Test that function to determine proband sex works
+        '''
+        proband_sex = extract_proband_sex(self.proband)
+        assert proband_sex == "46_xy"
+
+    def test_proband_phenotype_extraction(self):
+        '''
+        Test that the function to extract proband phenotypes works
+        '''
+        phenotype_list = extract_proband_phenotypes(self.proband)
+        assert phenotype_list == ["HP:0000119", "HP:0000121", "HP:0000377"]
+
+    def test_proband_variant_extraction(self):
+        '''
+        Test that the function to extract proband variants works
+        '''
+        variant_list = extract_proband_variants(self.interpretation)
+        assert variant_list == [
+            {'variant_id': "1:10108:C:CT",
+            'type': 'INDEL',
+            'zygosity': '0/1'},
+            {'variant_id': "1:927003:C:T",
+            'type': 'SNV',
+            'zygosity': '1|1'}
+        ]
+
+    def test_case_json_formatted_correctly(self):
+        '''
+        Test that the case dictionary is formatted correctly
+        '''
+        case_dictionary = format_required_data_into_case_json(
+            self.proband,
+            "46_xy",
+            ["HP:0000119", "HP:0000121", "HP:0000377"],
+            [{'variant_id': "1:10108:C:CT",
+            'type': 'INDEL',
+            'zygosity': '0/1'}]
+            )
+        assert case_dictionary == {
+        'sex': "46_xy",
+        'clinical_reference': "12345",
+        'phenotype_list': ["HP:0000119", "HP:0000121", "HP:0000377"],
+        'variant_list': [{
+            'variant_id': "1:10108:C:CT",
+            "type": 'INDEL',
+            'zygosity': '0/1'}]
+        }
 
 
 if __name__ == "__main__":
-    #opencga = TestOpenCGA()
+    opencga = TestOpenCGA()
     decipher = TestDecipher()
